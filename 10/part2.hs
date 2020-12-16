32dd42ad24435afae633285bdea4cfc94d3a585f
Jay <jay.invariant@gmail.com>
2020-12-16 14:37:24 -0800

    readability

--- 10/part2.hs
+++ 10/part2.hs
@@ -8,10 +8,11 @@ countArrangements diffs = last table
   where table = [arrs n | n <- [0..length diffs]]
         arrs 0 = 1
         arrs n = let merge0 = table !! (n - 1)
-                     merge1 = if n >= 2 && diffs !! (n - 1)
-                                 + diffs !! (n - 2) <= 3
+                     merge1 = if n >= 2 &&
+                                 diffs !! (n - 1) + diffs !! (n - 2) <= 3
                               then table !! (n - 2) else 0
-                     merge2 = if n >= 3 && diffs !! (n - 1) + diffs !! (n - 2)
+                     merge2 = if n >= 3 &&
+                                 diffs !! (n - 1) + diffs !! (n - 2)
                                  + diffs !! (n - 3) <= 3
                               then table !! (n - 3) else 0
                  in merge0 + merge1 + merge2
