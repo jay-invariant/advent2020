c50ea7352ceaa96ef23a33c5c1426eadf6cfce0d
Jay <jay.invariant@gmail.com>
2020-12-17 10:30:20 -0800

    day 17 cleanup

--- 17/part2.hs
+++ 17/part2.hs
@@ -10,7 +10,7 @@ type Space = Array (Int, Int, Int, Int) Bool
 -- Coords are all (z, y, x), makes it easier
 
 cycleSpace :: Space -> Space
-cycleSpace old = listArray newBounds [isActive p | p <- range newBounds]
+cycleSpace old = listArray newBounds $ isActive <$> range newBounds
   where isActive p = case (isOldActive p, activeNeighbors p) of
                        (True, 2) -> True
                        (True, 3) -> True
@@ -18,8 +18,8 @@ cycleSpace old = listArray newBounds [isActive p | p <- range newBounds]
                        _ -> False
         isOldActive p = inRange oldBounds p && old ! p
         activeNeighbors p@(w, z, y, x) =
-          sum [1 | x' <- [x-1..x+1], y' <- [y-1..y+1], z' <- [z-1..z+1], w' <- [w-1..w+1],
-               let p' = (w', z', y', x'), p /= p', isOldActive p']
+          sum [1 | p' <- range ((w-1, z-1, y-1, x-1), (w+1, z+1, y+1, x+1)),
+               p /= p', isOldActive p']
         oldBounds = bounds old
         newBounds = let ((w1, z1, y1, x1), (w2, z2, y2, x2)) = oldBounds
                     in ((w1-1, z1-1, y1-1, x1-1), (w2+1, z2+1, y2+1, x2+1))
@@ -29,7 +29,7 @@ initSpace str = listArray bnds $ concat lists
   where lists = fmap (fmap (=='#')) $ lines str
         rowLen = length $ head lists
         numRows = length lists
-        bnds = ((0,0,0,0), (0, 0, numRows - 1, rowLen - 1))
+        bnds = ((0, 0, 0, 0), (0, 0, numRows - 1, rowLen - 1))
 
 numActive :: Space -> Int
 numActive = length . filter id . toList
